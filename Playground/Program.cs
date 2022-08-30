ce27f1cf430462f0f92a042af91b6fa0304ba0ee
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2022-08-30 23:42:49 +0200

    Removed Playground project since api project is now self sufficient.

--- Playground/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using WebCrawler.Esportal;
-
-var crawler = new EsportalCrawler();
-Console.WriteLine(crawler.Bootstrap());
-//await Task.Delay(5000);
-//Console.WriteLine(crawler.Stop());
-
-Console.ReadLine();
