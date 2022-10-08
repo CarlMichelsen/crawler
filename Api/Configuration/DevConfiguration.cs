db32dd767d32c985ac23708b0812ab71df1281fc
Carl Lilholm Michelsen <carl.lilholm@protonmail.com>
2022-10-09 01:27:35 +0200

    Safe dev config.

--- Api/Configuration/DevConfiguration.cs
+++ Api/Configuration/DevConfiguration.cs
@@ -6,19 +6,31 @@ public class DevConfiguration
 
     public DevConfiguration()
     {
-        var projectDirectory = Environment.CurrentDirectory;
-        var solutionDirectory = Directory.GetParent(projectDirectory)?.FullName;
-        string[] lines = File.ReadAllLines($"{solutionDirectory}/.env");
-        foreach (var line in lines)
+        AttemptLoadLocalConfigurationFile();
+    }
+
+    private void AttemptLoadLocalConfigurationFile()
+    {
+        try
         {
-            if (string.IsNullOrWhiteSpace(line)) continue;
-            var splitIndex = line.IndexOf("=");
-            if (splitIndex == -1) continue;
-            var pair = new string[2] {line[..splitIndex], line[(splitIndex+1)..] };
-            if (pair.Length != 2) continue;
-            if (string.IsNullOrWhiteSpace(pair.ElementAtOrDefault(0))) continue;
-            if (string.IsNullOrWhiteSpace(pair.ElementAtOrDefault(1))) continue;
-            Configuration.Add(pair[0], pair[1]);
+            var projectDirectory = Environment.CurrentDirectory;
+            var solutionDirectory = Directory.GetParent(projectDirectory)?.FullName;
+            string[] lines = File.ReadAllLines($"{solutionDirectory}/.env");
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                var splitIndex = line.IndexOf("=");
+                if (splitIndex == -1) continue;
+                var pair = new string[2] {line[..splitIndex], line[(splitIndex+1)..] };
+                if (pair.Length != 2) continue;
+                if (string.IsNullOrWhiteSpace(pair.ElementAtOrDefault(0))) continue;
+                if (string.IsNullOrWhiteSpace(pair.ElementAtOrDefault(1))) continue;
+                Configuration.Add(pair[0], pair[1]);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Failed to load development configuration <{e?.Message ?? string.Empty}>");
         }
     }
 }
